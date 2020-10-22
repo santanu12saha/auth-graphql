f6c46b45fce77cf4f95ff950c34ca4f00581b047
santanu12saha <saha.santanu0217@gmail.com>
2020-10-22 08:06:10 +0530

    Adding attribute to fields in UserType.

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,12 +1,14 @@
 const graphql = require('graphql');
 const {
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLID
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
+        id: { type: GraphQLID },
         email: { type: GraphQLString }
     }
 });
